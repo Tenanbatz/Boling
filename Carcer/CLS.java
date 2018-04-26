bfba082fdb0b7448a30cc2f90a159ec6e6ae64d9
Tenanbatz <tenanbatz@gmail.com>
2018-04-26 11:27:05 -0700

    Delete CLS.java

--- Carcer/CLS.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-public class CLS 
-{
-    public static void main(String arg[]) throws IOException, InterruptedException 
-    {
-        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-    }
-}
