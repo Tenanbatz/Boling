7b0275b1ace8e2fc0b7cc9ecb0203fb50eec5446
Tenanbatz <tenanbatz@gmail.com>
2018-05-02 07:28:14 -0700

    Update User.java

--- Carcer/User.java
+++ Carcer/User.java
@@ -9,6 +9,12 @@ public class User
 {
     public User()
     {
-        
+        System.out.println("Welcome to the endless dungeon Carcer.");
+        System.out.println("In this dungeon you will encounter different enemies within each room.");
+        System.out.println("Each floor will have a random amount of rooms, no floor is the same.");
+        System.out.println("Your goal is to reach the bottom of the dungeon, but the thing is there is no bottom.");
+        System.out.println("You will fight enemies in turn based combat.");
+        System.out.println("Each class has a different set of skills and stats, they also contain unique passives.");
+        System.out.println("Don't forget to level up your character and good luck, you only have one chance.");
     }
 }
